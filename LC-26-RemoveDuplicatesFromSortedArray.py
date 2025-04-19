7aa448ec1b3e2debc391cee8b86335ee32231eeb
levani-b <levanbirchadze@gmail.com>
2025-04-19 16:53:22 +0400

    LC 26 Remove Duplicates from Sorted Array

--- LC-26-RemoveDuplicatesFromSortedArray.py
+++ LC-26-RemoveDuplicatesFromSortedArray.py
@@ -1,14 +1,12 @@
+# LeetCode Problem: 26. Remove Duplicates From Sorted Array
 # https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-def removeDuplicates( nums):
+
+def removeDuplicates(nums):
         l = 1
-        for r in range(1,len(nums)):
-                if nums[r] != nums[r-1]:
-                        nums[l] = nums[r]
-                        l+=1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l+= 1
+                
         return l
-
-
-# def removeDuplicates(nums):
-#         nums[:] = sorted(set(nums))
-#         return len(nums)
