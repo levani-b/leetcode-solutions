6c1c73b1d8ba71f1dce9d067fda515f3b91b492a
levani-b <levanbirchadze@gmail.com>
2025-05-18 20:14:01 +0400

    LC 443 String Compression

---
+++ LC-0443-StringCompression.py
@@ -0,0 +1,28 @@
+# LeetCode Problem: 443. String Compression
+# https://leetcode.com/problems/string-compression/
+
+from typing import List
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write = 0
+        read = 0
+
+        while read < len(chars):
+            char = chars[read]
+            count = 0
+
+            while read < len(chars) and chars[read] == char:
+                read += 1
+                count += 1
+
+            chars[write] = char
+            write += 1
+
+            if count > 1:
+                for c in str(count):
+                    chars[write] = c
+                    write += 1
+            
+        return write
