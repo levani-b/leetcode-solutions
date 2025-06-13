b10cc174d8395af5462329ab95a084a55bd5d693
levani-b <levanbirchadze@gmail.com>
2025-06-13 10:28:37 +0400

    LC 75 Sort Colors

--- LC-0075-SortColors.py
+++ LC-0075-SortColors.py
@@ -3,27 +3,47 @@
 
 from typing import List
 
-
-def sortColors(nums: List[int]) -> None:
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        red = 0
-        white = 0
-        blue = 0
-        for num in nums:
-            if num == 0:
-                red+=1
-            elif num == 1:
-                white += 1
+        left = 0
+        right = len(nums) - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[i], nums[left] = nums[left], nums[i]
+                i += 1
+                left += 1
+            elif nums[i] == 2:
+                nums[i], nums[right] = nums[right], nums[i]
+                right -= 1
             else:
-                blue += 1
-        for i in range(len(nums)):
-            if red >0:
-                nums[i] = 0
-                red -= 1
-            elif white >0:
-                nums[i] = 1
-                white -= 1
-            else:
-                nums[i] = 2
+                i += 1
+
+
+# class Solution:
+#     def sortColors(nums: List[int]) -> None:
+#             """
+#             Do not return anything, modify nums in-place instead.
+#             """
+#             red = 0
+#             white = 0
+#             blue = 0
+#             for num in nums:
+#                 if num == 0:
+#                     red+=1
+#                 elif num == 1:
+#                     white += 1
+#                 else:
+#                     blue += 1
+#             for i in range(len(nums)):
+#                 if red >0:
+#                     nums[i] = 0
+#                     red -= 1
+#                 elif white >0:
+#                     nums[i] = 1
+#                     white -= 1
+#                 else:
+#                     nums[i] = 2
