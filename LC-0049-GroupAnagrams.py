21188245ade06e23fc4654376d716549ac1cd2fd
levani-b <levanbirchadze@gmail.com>
2025-10-07 11:21:46 +0400

    LC 49 Group Anagrams

--- LC-0049-GroupAnagrams.py
+++ LC-0049-GroupAnagrams.py
@@ -1,16 +1,15 @@
 # LeetCode Problem: 49. Group Anagrams
 # https://leetcode.com/problems/group-anagrams/
 
-from collections import defaultdict
-from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = {}
+        for string in strs:
+            sorted_str = ''.join(sorted(string))
 
+            if sorted_str not in groups:
+                groups[sorted_str] = []
 
-def groupAnagrams(strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord('a')] += 1
-            res[tuple(count)].append(s)
-        return list(res.values())
-        
+            groups[sorted_str].append(string)
+
+        return list(groups.values())
