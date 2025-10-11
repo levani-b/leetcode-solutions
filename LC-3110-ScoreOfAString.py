c7197a743f0274de31473cfedb59f0abb01fd152
levani-b <levanbirchadze@gmail.com>
2025-10-11 13:03:25 +0400

    LC 3110 Score of a String

--- LC-3110-ScoreOfAString.py
+++ LC-3110-ScoreOfAString.py
@@ -1,13 +1,11 @@
-# LeetCode Problem: 3110. Score Of A String
+# LeetCode Problem: 3110. Score of a String
 # https://leetcode.com/problems/score-of-a-string/
 
-def scoreOfString(self, s: str) -> int:
-    ascii_arr = []
-    for char in s:
-        ascii_arr.append(ord(char))
-    
-    res = 0
-    for i in range(len(ascii_arr) -1):
-        num = abs(ascii_arr[i] - ascii_arr[i+1])
-        res += num
-    return res
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s) - 1):
+            curr_value = abs(ord(s[i]) - ord(s[i+1]))
+            score += curr_value
+        
+        return score
