0f41522b905a87cf38e46b73ba7e9a93540de3b9
levani-b <levanbirchadze@gmail.com>
2025-10-11 13:09:57 +0400

    LC 1929 Concatenation of Array

--- LC-1929-ConcatenationOfArray.py
+++ LC-1929-ConcatenationOfArray.py
@@ -1,9 +1,20 @@
-# LeetCode Problem: 1929. Concatenation Of Array
+# LeetCode Problem: 1929. Concatenation of Array
 # https://leetcode.com/problems/concatenation-of-array/
 
-def getConcatenation(nums):
-        result = []
-        for _ in range(2):
-            for num in nums:
-                result.append(num)
-        return result
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums) * 2
+        for i in range(len(nums)):
+            ans[i] = nums[i]
+            ans[i + len(nums)] = nums[i]
+        
+        return ans
+
+
+
+# def getConcatenation(nums):
+#         result = []
+#         for _ in range(2):
+#             for num in nums:
+#                 result.append(num)
+#         return result
