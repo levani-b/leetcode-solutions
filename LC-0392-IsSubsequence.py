bb88781f73dac12c4c6ace07e1f206cb42defa7a
levani-b <levanbirchadze@gmail.com>
2025-10-11 13:27:21 +0400

    LC 392 Is Subsequence

--- LC-0392-IsSubsequence.py
+++ LC-0392-IsSubsequence.py
@@ -1,13 +1,12 @@
 # LeetCode Problem: 392. Is Subsequence
 # https://leetcode.com/problems/is-subsequence/
 
-def isSubsequence(self, s: str, t: str) -> bool:
-    res = ''
-    i,j = 0,0
-
-    while j < len(t) and i < len(s):
-        if s[i] == t[j]:
-            res += t[j]
-            i += 1
-        j+=1
-    return res == s
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while j < len(t) and i < len(s):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+ 
+        return i == len(s)
