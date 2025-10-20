4ec327519e484d8e2c3d49503c01e57cf9f049e9
levani-b <levanbirchadze@gmail.com>
2025-10-20 14:02:06 +0400

    LC 2011 Final Value of Variable After Performing Operations

---
+++ LC-2011-FinalValueOfVariableAfterPerformingOperations.py
@@ -0,0 +1,12 @@
+# LeetCode Problem: 2011. Final Value Of Variable After Performing Operations
+# https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for operation in operations:
+            if '-' in operation:
+                x -= 1
+            else:
+                x += 1
+        return x
