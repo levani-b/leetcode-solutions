6a1682d102417832ed8f8de8c390f193331a27f9
levani-b <levanbirchadze@gmail.com>
2025-10-21 12:16:45 +0400

    LC 605 Can Place Flowers

--- LC-0605-CanPlaceFlowers.py
+++ LC-0605-CanPlaceFlowers.py
@@ -7,29 +7,21 @@ from typing import List
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         count = 0
-        length = len(flowerbed)
-        for i in range(length):
-            if flowerbed[i] == 0:
-                if i == 0:
-                    empty_left = True
-                else:
-                    if flowerbed[i-1] == 0:
-                        empty_left == True
-                    else:
-                        empty_left == False
 
-                if i == length - 1:
-                    empty_right = True
-                else:
-                    if flowerbed[i + 1] == 0:
-                        empty_right = True
-                    else:
-                        empty_right = False
+        if n == 0:
+            return True
+            
+        for i in range(len(flowerbed)):
+            left_empty, right_empty = False, False
+            if i == 0 or flowerbed[i - 1] == 0:
+                left_empty = True
 
-                if empty_left and empty_right:
-                    flowerbed[i] = 1
-                    count += 1
-                    if count >= n:
-                        return True
+            if i == len(flowerbed) - 1 or flowerbed[i+1] == 0:
+                right_empty = True
 
-        return count >= n
+            if left_empty and flowerbed[i] == 0 and right_empty:
+                flowerbed[i] = 1
+                n -= 1
+                if n == 0:
+                    return True
+        return False
