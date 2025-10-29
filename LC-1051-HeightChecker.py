756ab4e84017b5fa5b1a34a018443d251559de1e
levani-b <levanbirchadze@gmail.com>
2025-10-29 22:43:47 +0400

    LC 1051 Height Checker

---
+++ LC-1051-HeightChecker.py
@@ -0,0 +1,16 @@
+# LeetCode Problem: 1051. Height Checker
+# https://leetcode.com/problems/height-checker/
+
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        expected = sorted(heights)
+
+        count = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count += 1
+        
+        return count
