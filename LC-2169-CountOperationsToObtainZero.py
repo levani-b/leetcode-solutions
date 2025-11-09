eaef10751c73b8302c20602061df1a784b554a80
levani-b <levanbirchadze@gmail.com>
2025-11-09 14:40:57 +0400

    LC 2169 Count Operations to Obtain Zero

---
+++ LC-2169-CountOperationsToObtainZero.py
@@ -0,0 +1,15 @@
+# LeetCode Problem: 2169. Count Operations to Obtain Zero
+# https://leetcode.com/problems/count-operations-to-obtain-zero/
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        while num1 > 0 and num2 > 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            count +=1
+        
+        return count
+       
