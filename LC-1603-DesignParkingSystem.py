656aa28adfe7297b17b5daaaf9248169de421a30
levani-b <levanbirchadze@gmail.com>
2025-11-11 18:54:17 +0400

    LC 1603 Design Parking System

---
+++ LC-1603-DesignParkingSystem.py
@@ -0,0 +1,13 @@
+# LeetCode Problem: 1603. Design Parking System
+# https://leetcode.com/problems/design-parking-system/
+
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.spaces = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.spaces[carType - 1] > 0:
+            self.spaces[carType - 1] -= 1
+            return True
+        return False
