055bbbc723350e1a911fb4a6803ad94642f1ebdf
levani-b <levanbirchadze@gmail.com>
2025-11-19 13:52:25 +0400

    LC 263 Ugly Number

---
+++ LC-0263-UglyNumber.py
@@ -0,0 +1,13 @@
+# LeetCode Problem: 263. Ugly Number
+# https://leetcode.com/problems/ugly-number/
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+
+        for factor in [2, 3, 5]:
+            while n % factor == 0:
+                n //= factor
+        
+        return n == 1
