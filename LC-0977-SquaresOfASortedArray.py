1773783e12c036c6bb3cf53669c7e173574b0ead
levani-b <levanbirchadze@gmail.com>
2026-01-30 14:12:16 +0400

    LC 977 Squares of a Sorted Array

--- LC-0977-SquaresOfASortedArray.py
+++ LC-0977-SquaresOfASortedArray.py
@@ -1,21 +1,38 @@
 # LeetCode Problem: 977. Squares of a Sorted Array
 # https://leetcode.com/problems/squares-of-a-sorted-array/
 
-from typing import List
-
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = []
+        res = [0] * len(nums)
         l = 0
         r = len(nums) - 1
+        pos = len(nums) - 1
 
         while l <= r:
             if nums[l] ** 2 > nums[r] ** 2:
-                res.append(nums[l] ** 2)
+                res[pos] = nums[l] ** 2
                 l += 1
             else:
-                res.append(nums[r] ** 2)
+                res[pos] = nums[r] ** 2
                 r -= 1
+            pos -= 1
+
+        return res
+    
+    
+# class Solution:
+#     def sortedSquares(self, nums: List[int]) -> List[int]:
+#         res = []
+#         l = 0
+#         r = len(nums) - 1
+
+#         while l <= r:
+#             if nums[l] ** 2 > nums[r] ** 2:
+#                 res.append(nums[l] ** 2)
+#                 l += 1
+#             else:
+#                 res.append(nums[r] ** 2)
+#                 r -= 1
         
-        return res[::-1]
+#         return res[::-1]
