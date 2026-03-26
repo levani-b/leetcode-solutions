e67a368c6b12193df2e2d9cb305b8761726c19f7
levani-b <levanbirchadze@gmail.com>
2026-03-26 13:58:29 +0400

    LC 867 Transpose Matrix

---
+++ LC-0867-TransposeMatrix.py
@@ -0,0 +1,13 @@
+# LeetCode Problem: 867. Transpose Matrix
+# https://leetcode.com/problems/transpose-matrix/
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m, n = len(matrix), len(matrix[0])
+        res = [[0] * m for _ in range(n)] 
+
+        for i in range(m):
+            for j in range(n):
+                res[j][i] = matrix[i][j]
+        
+        return res
