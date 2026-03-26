47b94bf99b2618e55b541559d84d5d64f9395cf1
levani-b <levanbirchadze@gmail.com>
2026-03-26 14:16:42 +0400

    LC 1365 How Many Numbers Are Smaller Than the Current Number

--- LC-1365-HowManyNumbersAreSmallerThanTheCurrentNumber.py
+++ LC-1365-HowManyNumbersAreSmallerThanTheCurrentNumber.py
@@ -17,3 +17,16 @@ class Solution:
                 result[orig_idx] = i
         
         return result
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            count = 0
+            for j in range(len(nums)):
+                if nums[j] < nums[i]:
+                    count += 1
+            res[i] = count
+        
+        return res
