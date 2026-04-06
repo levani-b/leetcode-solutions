1decc53172ae3ca889a13583ae5ac03ac3d85b31
levani-b <levanbirchadze@gmail.com>
2026-04-06 19:15:58 +0400

    LC 169 Majority Element

--- LC-0169-MajorityElement.py
+++ LC-0169-MajorityElement.py
@@ -5,6 +5,22 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if candidate == num:
+                count += 1
+            else:
+                count -= 1
+            
+        return candidate
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         maj = len(nums) // 2
         freq = {}
         for num in nums:
