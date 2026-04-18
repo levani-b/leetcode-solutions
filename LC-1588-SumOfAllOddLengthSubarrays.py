482b2d74d77adf229e7dc906ab1cd3071bdbe1e2
levani-b <levanbirchadze@gmail.com>
2026-04-18 14:51:19 +0400

    LC 1588 Sum of All Odd Length Subarrays

--- LC-1588-SumOfAllOddLengthSubarrays.py
+++ LC-1588-SumOfAllOddLengthSubarrays.py
@@ -3,6 +3,24 @@
 
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        prefix_sum = []
+        curr_total = 0
+        for num in arr:
+            curr_total += num
+            prefix_sum.append(curr_total)
+
+        res = 0
+        for i in range(len(arr)):
+            for j in range(i, len(arr), 2):
+                if i == 0:
+                    res += prefix_sum[j]
+                else:
+                    res += prefix_sum[j] - prefix_sum[i - 1]
+        return res
+    
+    
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
         sum_of_subarray = 0
         n = len(arr)
         for length in range(1,n + 1, 2):
