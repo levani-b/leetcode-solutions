26099e1bd32f9327790e67630429259463f57297
levani-b <levanbirchadze@gmail.com>
2026-05-11 16:57:47 +0400

    LC 520 Detect Capital

---
+++ LC-0520-DetectCapital.py
@@ -0,0 +1,12 @@
+# LeetCode Problem: 520. Detect Capital
+# https://leetcode.com/problems/detect-capital/
+
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper():
+            return True
+        if word.islower():
+            return True
+        if word[0].isupper() and word[1:].islower():
+            return True
+        return False
