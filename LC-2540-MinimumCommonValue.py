85425d4d5ea35021545e2b825c535619d7804f5c
levani-b <levanbirchadze@gmail.com>
2026-05-19 17:27:55 +0400

    LC 2540 Minimum Common Value

---
+++ LC-2540-MinimumCommonValue.py
@@ -0,0 +1,16 @@
+# LeetCode Problem: 2540. Minimum Common Value
+# https://leetcode.com/problems/minimum-common-value/
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        i, j = 0, 0
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] > nums2[j]:
+                j += 1
+            else:
+                i += 1
+
+        return -1
